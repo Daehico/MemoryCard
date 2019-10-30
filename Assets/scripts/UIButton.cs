9419b2613a1acffe22c0a9e4a9b823236610219c
Дмитрий Дудин <empire_95@mail.ru>
2019-10-30 17:54:46 +0300

    Memory

---
+++ Assets/scripts/UIButton.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIButton : MonoBehaviour {
+
+    [SerializeField] private GameObject targetObject;
+    [SerializeField] private string targerMessage;
+    public Color highlightColor = Color.cyan;
+
+    public void OnMouseOver()
+    {
+        SpriteRenderer sprire = GetComponent<SpriteRenderer>();
+        if (sprire != null)
+        {
+            sprire.color = highlightColor;
+
+        } 
+    }
+
+    public void OnMouseExit()
+    {
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        if (sprite != null)
+        {
+            sprite.color = Color.white;
+        }
+    }
+
+    public void OnMouseDown()
+    {
+        transform.localScale = Vector3.one;
+        if (targetObject != null)
+        {
+            targetObject.SendMessage(targerMessage);
+        }
+    }
+}
